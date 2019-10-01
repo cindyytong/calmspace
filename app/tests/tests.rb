d0c79adb5c91a24e569673ee60e6e0def822a070
“cindyytong” <cindytong@Cindys-MacBook-Pro.local>
2019-10-01 18:21:44 -0400

    Remove test file

--- app/tests/tests.rb
+++ app/tests/tests.rb
@@ -1,11 +1,11 @@
-# Users 
-User1 = User.create!(username: 'user1', password: '12345678', over_age_13: true, email: 'user1@gmail.com')
-User2 = User.create!(username: 'user2', password: '12345678', over_age_13: true, email: 'user2@gmail.com')
+# # Users 
+# User1 = User.create!(username: 'user1', password: '12345678', over_age_13: true, email: 'user1@gmail.com')
+# User2 = User.create!(username: 'user2', password: '12345678', over_age_13: true, email: 'user2@gmail.com')
 
-user1 = User.new 
-user1.username = 'user1'
-user1.password = '12345678'
-user1.over_age_13 = true 
-user1.email = 'user1@gmail.com'
-user1.save!
+# user1 = User.new 
+# user1.username = 'user1'
+# user1.password = '12345678'
+# user1.over_age_13 = true 
+# user1.email = 'user1@gmail.com'
+# user1.save!
 
